f79878158ebe54aa5b3e751232932f2c8b4851d8
edcamero <39476414+edcamero@users.noreply.github.com>
2020-01-05 15:59:15 -0500

    sockets se comunican exitosamente para trasmitir mensajes

--- api/controllers/MensajeController.js
+++ api/controllers/MensajeController.js
@@ -6,6 +6,18 @@
  */
 
 module.exports = {
+
+
+    create: function(req, res) {
+            console.log(req.body);
+        sails.sockets.join(req, 'funSockets');
+
+        // Broadcast a notification to all the sockets who have joined
+        // the "funSockets" room, excluding our newly added socket:
+        sails.sockets.broadcast('funSockets', 'create', req.body, req);
+
+    }
+    
   
 
 };
